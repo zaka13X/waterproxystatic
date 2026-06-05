555013235b0c29a847c18085bcdca380639c3e2e
zaka13 <105675209+zaka13X@users.noreply.github.com>
2026-06-05 11:08:00 -0500

    Update uv.config.js

--- active/uv/uv.config.js
+++ active/uv/uv.config.js
@@ -1,10 +1,11 @@
 self.__uv$config = {
-    prefix: '/active/go/',
-    bare:'https://32867gd198764dg23vd23198746v4d69182736dvussis7.9882136.xyz/',
-    encodeUrl: Ultraviolet.codec.xor.encode,
-    decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/active/uv/uv.handler.js',
-    bundle: '/active/uv/uv.bundle.js',
-    config: '/active/uv/uv.config.js',
-    sw: '/active/uv/uv.sw.js',
+    prefix: '/active/go/', // Keep this as /active/go/ since your browser URL routes to /active/go/
+    bare: 'https://32867gd198764dg23vd23198746v4d69182736dvussis7.9882136.xyz/', // Your functional Bare server
+    encodeUrl: Ultramarine.codec.xor.encode,
+    decodeUrl: Ultramarine.codec.xor.decode,
+    handler: '/active/uv.handler.js',
+    client: '/active/uv.client.js',
+    bundle: '/active/uv.bundle.js',
+    config: '/active/uv.config.js',
+    sw: '/active/uv-sw.js', // This must map directly to where GitHub Pages maps the file
 };
